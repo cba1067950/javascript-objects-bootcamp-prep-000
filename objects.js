47f0ebd17f6a9060da1faacab73612e548e41b7a
Joe Jamsky <cba1067950@yahoo.com>
2019-04-03 00:48:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,8 +2,8 @@ var playlist = {ArtistName: "songTitle"};
 
  //Object.assign()
 
-function updatePlaylist (playlist, name, song){
-  playlist[name] = song;
+function updatePlaylist (list, name, song){
+  list[name] = song;
   return playlist;
 }
 
